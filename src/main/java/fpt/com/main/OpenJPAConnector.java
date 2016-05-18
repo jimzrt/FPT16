400c2f893fde676e42a4c1ee53945c8f23ebee8b
james <james@james-pc>
2016-05-19 01:35:01 +0200

    update openjpaConnector wegen openjpa 2.4 update

--- src/main/java/fpt/com/main/OpenJPAConnector.java
+++ src/main/java/fpt/com/main/OpenJPAConnector.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 import org.apache.openjpa.persistence.OpenJPAPersistence;
@@ -20,9 +21,7 @@ public class OpenJPAConnector {
 		if (product.getId() == -1) {
 			product.setId(0);
 			if (fac == null)
-				fac = getWithoutConfig();
-			// fac = Persistence.createEntityManagerFactory(
-			// "openjpa", System.getProperties());
+				fac = Persistence.createEntityManagerFactory("openjpa", System.getProperties());
 
 			if (e == null)
 				e = fac.createEntityManager();
@@ -39,9 +38,7 @@ public class OpenJPAConnector {
 		ArrayList<Product> products = new ArrayList<Product>();
 
 		if (fac == null)
-			fac = getWithoutConfig();
-		// fac = Persistence.createEntityManagerFactory(
-		// "openjpa");
+			fac = Persistence.createEntityManagerFactory("openjpa");
 		if (e == null)
 			e = fac.createEntityManager();
 
@@ -67,25 +64,6 @@ public class OpenJPAConnector {
 			e.close();
 		if (fac != null)
 			fac.close();
-
-	}
-
-	public static EntityManagerFactory getWithoutConfig() {
-
-		Map<String, String> map = new HashMap<String, String>();
-
-		map.put("openjpa.ConnectionURL",
-				"jdbc:postgresql://java.is.uni-due.de/ws1011");
-		map.put("openjpa.ConnectionDriverName", "org.postgresql.Driver");
-		map.put("openjpa.ConnectionUserName", "ws1011");
-		map.put("openjpa.ConnectionPassword", "ftpw10");
-		map.put("openjpa.RuntimeUnenhancedClasses", "supported");
-		map.put("openjpa.jdbc.SynchronizeMappings", "false");
-
-		map.put("openjpa.MetaDataFactory",
-				"jpa(Types=" + Product.class.getName() + ")");
-
-		return OpenJPAPersistence.getEntityManagerFactory(map);
 
 	}
 
