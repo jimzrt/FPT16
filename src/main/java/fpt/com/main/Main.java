ea5075fdf546822cad645d8d1adf66237f5109ee
jimzrt <james.tophoven@gmail.com>
2016-05-19 14:23:17 +0200

    Beende Programm, wenn eine View geschlossen wird

--- src/main/java/fpt/com/main/Main.java
+++ src/main/java/fpt/com/main/Main.java
@@ -5,6 +5,7 @@ import fpt.com.main.Controller.ControllerShop;
 import fpt.com.main.Views.ViewCustomer;
 import fpt.com.main.Views.ViewShop;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -22,32 +23,35 @@ public class Main extends Application {
 		ModelShop model = new ModelShop();
 
 		ViewShop view = new ViewShop();
-		//ViewCustomer view2 = new ViewCustomer();
+		ViewCustomer view2 = new ViewCustomer();
 
 		ControllerShop control = new ControllerShop();
-		//ControllerCustomer control2 = new ControllerCustomer();
+		ControllerCustomer control2 = new ControllerCustomer();
 
 		control.link(model, view);
-		//control2.link(model, view2);
+		control2.link(model, view2);
 
-		
-		
 		// JavaFX new
 		Scene scene = new Scene(view);
 		primaryStage.setTitle("Shop");
 		primaryStage.setScene(scene);
+		primaryStage.setOnCloseRequest(e -> {
+			Platform.exit();
+		});
 
 		// JavaFX new
 
-/*		Stage stage2 = new Stage();
+		Stage stage2 = new Stage();
 		stage2.setTitle("Customer");
 		Scene scene2 = new Scene(view2);
 		stage2.setScene(scene2);
 		stage2.setX(primaryStage.getX() - stage2.getWidth());
 		stage2.setY(primaryStage.getY() - stage2.getHeight());
+		stage2.setOnCloseRequest(e -> {
+			Platform.exit();
+		});
 		stage2.show();
-		*/
-		
+
 		primaryStage.show();
 	}
 
