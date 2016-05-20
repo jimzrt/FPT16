fcf1b3c9a654f571a7baaae9df227cda6cf5b707
jimzrt <james.tophoven@gmail.com>
2016-05-20 03:14:48 +0200

    UDP Client und Server für Zeitübertragung hinzugefügt

---
+++ src/main/java/fpt/com/main/Network/Client.java
@@ -0,0 +1,38 @@
+package fpt.com.main.Network;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Client implements Runnable {
+
+	public String text = "";
+	public DatagramPacket packetReq;
+	public DatagramPacket packetResp;
+	public String lastTime = "";
+
+	public Client() { 
+		InetAddress ia = null;
+		try {
+			ia = InetAddress.getByName("localhost");
+		} catch (UnknownHostException e2) {
+			e2.printStackTrace();
+		}
+		String command = "time";
+		byte buffer[] = null;
+		buffer = command.getBytes();
+
+		// Paket mit der Anfrage vorbereiten
+		packetReq = new DatagramPacket(buffer, buffer.length, ia, 6667);
+
+		byte answer[] = new byte[1024];
+		packetResp = new DatagramPacket(answer, answer.length);
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+}
